f13e704da1072ebd2be4e7d228e26ab906aa3ba3
Rommel Lardizabal <rlardizabal@iom.int>
2017-04-26 11:54:36 +0800

    Modified class and java file

--- src/executionEngine/DriverScript.java
+++ src/executionEngine/DriverScript.java
@@ -6,20 +6,50 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import config.ActionKeywords;
+import utility.ExcelUtils;
+
 public class DriverScript {
 
 	private static WebDriver driver = null;
-	public static void main(String[] args) {
-		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("http://www.store.demoqa.com");
-		driver.findElement(By.xpath(".//*[@id='account']/a")).click();
-	    driver.findElement(By.id("log")).sendKeys("testuser121212"); 
-	    driver.findElement(By.id("pwd")).sendKeys("6DuHQyQbkx6GJ6$V");
-	    driver.findElement(By.id("login")).click();
-	    driver.findElement (By.xpath(".//*[@id='account_logout']/a")).click();
-	    driver.quit();
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		
+		
+		String sPath = "//Selenium-ToolsQA//src//dataEngine//DataEngine.xlsx";
+		ActionKeywords ActionKeywords = new ActionKeywords();
+		
+		//Pass the PathName so the util class can read the file
+		ExcelUtils excelUtilObj = new ExcelUtils();
+		excelUtilObj.setExcelFile(sPath, "Test Steps");
+		
+		for(int i=1;i<=9;i++){
+			String ActionKeywordString = excelUtilObj.getCellData(i, 3);
+			
+			//Comparing the value of Excel cell with all the project keywords
+			if(ActionKeywordString.equals("openBrowser")){
+	                    //This will execute if the excel cell value is 'openBrowser'
+				//Action Keyword is called here to perform action
+				ActionKeywords.openBrowser();}
+			else if(ActionKeywordString.equals("navigate")){
+				ActionKeywords.navigate();}
+			else if(ActionKeywordString.equals("click_MyAccount")){
+				ActionKeywords.click_MyAccount();}
+			else if(ActionKeywordString.equals("input_Username")){
+				ActionKeywords.input_Username();}
+			else if(ActionKeywordString.equals("input_Password")){
+				ActionKeywords.input_Password();}
+			else if(ActionKeywordString.equals("click_Login")){
+				ActionKeywords.click_Login();}
+			else if(ActionKeywordString.equals("waitFor")){
+				ActionKeywords.waitFor();}
+			else if(ActionKeywordString.equals("click_Logout")){
+				ActionKeywords.click_Logout();}
+			else if(ActionKeywordString.equals("closeBrowser")){
+				ActionKeywords.closeBrowser();}
+			
+		}
+		
+
 
 	}
 
